9b19610edc2fa0547455bb9a7dc470b985c1ab6c
Maciej Olejnik <maciekolej@gmail.com>
2022-03-28 23:45:36 +0200

    feat: Add initial library boilerplate

---
+++ example/src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import { ExampleComponent } from '@bit-about/state'
+import '@bit-about/state/dist/index.css'
+
+const App = () => {
+  return <ExampleComponent text='@bit-about Example' />
+}
+
+export default App
