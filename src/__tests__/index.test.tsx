7cf85e757bacbc5c46ec47f7023bb48548d3e7d5
Maciej Olejnik <maciekolej@gmail.com>
2022-03-30 23:50:43 +0200

    feat: add basic rerender test

--- src/__tests__/index.test.tsx
+++ src/__tests__/index.test.tsx
@@ -1,16 +1,15 @@
 import * as React from 'react'
-import { render } from '@testing-library/react'
+import { fireEvent, getByTestId, render } from '@testing-library/react'
 import state from '../state'
 
 // Counter Component
-const getCounterText = (count: number) => `[rendered:${count}]`
 const Counter = () => {
   const renderCounter = React.useRef(0)
   renderCounter.current = renderCounter.current + 1
-  return getCounterText(renderCounter.current)
+  return <p role='counter'>{renderCounter.current}</p>
 }
 
-test('basic usage', () => {
+test('Basic usage', () => {
   const VALUE = 'Alice'
   const [Provider, useBase] = state(() => {
     const [value] = React.useState(VALUE)
@@ -19,7 +18,7 @@ test('basic usage', () => {
 
   const Child = () => {
     const { value } = useBase()
-    return <>{value}</>
+    return <p role='value'>{value}</p>
   }
 
   const App = () => (
@@ -29,4 +28,32 @@ test('basic usage', () => {
   )
   const { getByText } = render(<App />)
   expect(getByText(VALUE)).toBeDefined()
+})
+
+test('Rerender', () => {
+  const [Provider, useBase] = state(() => {
+    const [value, setValue] = React.useState(0)
+    return { value, setValue }
+  })
+
+  const Button = () => {
+    const { setValue } = useBase()
+    return <button role='button' onClick={() => setValue(5)} />
+  }
+
+  const Child = () => {
+    const { value } = useBase()
+    return <p role='value'>{value}</p>
+  }
+
+  const App = () => (
+    <Provider>
+      <Button />
+      <Child />
+    </Provider>
+  )
+  const { getByRole } = render(<App />)
+  expect(getByRole('value').textContent).toEqual('0')
+  fireEvent.click(getByRole('button'))
+  expect(getByRole('value').textContent).toEqual('5')
 })
