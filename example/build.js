2cdd68fad732aafff67e4a5f33330bbc80fd94a8
Maciej Olejnik <maciekolej@gmail.com>
2022-05-08 19:46:24 +0200

    feat: add prerendering

--- example/build.js
+++ example/build.js
@@ -7,8 +7,9 @@ const config = defaults.__get__('config')
  * @link https://kentcdodds.com/blog/profile-a-react-app-for-performance#disable-function-name-mangling
  */
 config.optimization.minimizer[0].options.extractComments = false
-config.optimization.minimizer[0].options.minimizer.options = true
-config.optimization.minimizer[0].options.minimizer.options = true
+config.optimization.minimizer[0].options.minimizer.options.keep_classnames = true
+config.optimization.minimizer[0].options.minimizer.options.keep_fnames = true
+config.optimization.minimizer[0].options.minimizer.options.output.comments = true
 
 // Force development env
 config.plugins[4].definitions['process.env'].NODE_ENV = '"development"'
--- example/src/index.js
+++ example/src/index.js
@@ -4,7 +4,12 @@ import './index.css'
 import App from './components/App/App'
 import reportWebVitals from './reportWebVitals'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const rootElement = document.getElementById('root')
+if (rootElement.hasChildNodes()) {
+  ReactDOM.hydrate(<App />, rootElement)
+} else {
+  ReactDOM.render(<App />, rootElement)
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
