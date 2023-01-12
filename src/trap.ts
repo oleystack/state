be7ea1793476397288d4bccca598c4fcc5f69c68
Maciej Olejnik <maciekolej@gmail.com>
2023-01-12 21:11:00 +0100

    feat: make trap stable

--- src/trap.ts
+++ src/trap.ts
@@ -1,63 +1,65 @@
-class Trap extends Function {
-  constructor() {
-    super()
-    return new Proxy(this, {
-      construct() {
-        return new Trap()
-      },
-      get(target, propertyName) {
-        return target[propertyName] ?? new Trap()
-      },
-      getOwnPropertyDescriptor() {
-        return {
-          writable: false,
-          configurable: true,
-          enumerable: true,
-          value: new Trap()
-        }
-      },
-      getPrototypeOf() {
-        return trap
-      },
+export const trap: any = (() => {
+  class Trap extends Function {
+    constructor() {
+      super()
+      return new Proxy(this, {
+        construct() {
+          return trap
+        },
+        get(target, propertyName) {
+          return target[propertyName] ?? trap
+        },
+        getOwnPropertyDescriptor() {
+          return {
+            writable: false,
+            configurable: true,
+            enumerable: true,
+            value: trap
+          }
+        },
+        getPrototypeOf() {
+          return trap
+        },
 
-      has() {
-        return false
-      },
-      isExtensible() {
-        return false
-      },
-      ownKeys() {
-        return []
-      },
-      apply: () => new Trap()
-    })
-  }
+        has() {
+          return false
+        },
+        isExtensible() {
+          return false
+        },
+        ownKeys() {
+          return []
+        },
+        apply: () => trap
+      })
+    }
 
-  _call() {
-    return new Trap()
-  }
+    _call() {
+      return trap
+    }
 
-  valueOf() {
-    return 0
-  }
-
-  toString() {
-    return `It's a trap!`
-  }
-
-  toInteger() {
-    return 0
-  }
-
-  [Symbol.toPrimitive](hint: string) {
-    if (hint === 'number') {
+    valueOf() {
       return 0
     }
-    if (hint === 'string') {
+
+    toString() {
       return `It's a trap!`
     }
-    return false
-  }
-}
 
-export const trap: any = new Trap()
+    toInteger() {
+      return 0
+    }
+
+    [Symbol.toPrimitive](hint: string) {
+      if (hint === 'number') {
+        return 0
+      }
+      if (hint === 'string') {
+        return `It's a trap!`
+      }
+      return false
+    }
+  }
+
+  return new Trap()
+})()
